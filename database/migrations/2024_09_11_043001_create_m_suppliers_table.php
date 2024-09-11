a2d16e25645eed6cf4f0f93347c16216b30f5eeb
Afifah Rahma <afifahhrahmaa03@gmail.com>
2024-09-11 13:00:57 +0700

    Migration Praktikum 1- 2

--- database/migrations/2024_09_11_043001_create_m_suppliers_table.php
+++ database/migrations/2024_09_11_043001_create_m_suppliers_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('m_suppliers', function (Blueprint $table) {
+            $table->id('supplier_id');
+            $table->string('supplier_nama');
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('m_suppliers');
     }
 };
