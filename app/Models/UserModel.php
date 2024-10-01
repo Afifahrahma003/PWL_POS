39304a4067f25836be6a95b7172f931de71d6769
Afifah Rahma <afifahhrahmaa03@gmail.com>
2024-10-01 23:47:35 +0700

    JS05 - Praktikum 3 Implementasi jQuery Datatable di AdminLTE

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,24 +6,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+use function Laravel\Prompts\password;
+
 class UserModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan UserModel
-    protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
-
-    // Kolom yang dapat diisi (mass assignable)
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
-
-    //modifikasi 
-    // protected $fillable = ['level_id', 'username', 'nama'];
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    /**
+     * The attributes that are mass assignable
+     * 
+     * @var array
+     */
+    protected $fillable = ['user_id', 'level_id', 'username', 'nama', 'password'];
 
     public function level(): BelongsTo
-{
-    return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-}
-
-
-    
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
 <div class ="card">
     <div class="card-header">
-        <h3 class="card-title">Hallo, apakabar</h3>
+        <h3 class="card-title">Hallo, apa kabar</h3>
         <div class="card-tools"></div>
     </div>
     <div class="card-body">
